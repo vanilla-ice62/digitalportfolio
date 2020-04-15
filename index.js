25ade7a36503d52ff2a4b97613ebf28496d0a017
vanilla-ice62 <63723555+vanilla-ice62@users.noreply.github.com>
2020-04-15 14:13:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let firstName;
+let lastName;
+let age = 60;
+let lightOn = true;
+
+function lightSwitch() {
+
+    if (lightOn == true) {
+        lightOn = false
+        console.log(lightOn)
+    }
+    else {
+        lightOn = true
+
+    console.log(lightOn)
+    }
+
+}
+
+lightSwitch()
+lightSwitch()
+lightSwitch()
+
+let numberOne;
+let numberTwo;
+let total;
+
+function addition(numberOne, numberTwo) {
+total= numberOne + numberTwo
+console.log(total)
+
+}
+
+addition(20, 30)
+addition(27, 57)
+addition(45, 45)
